e536f4202bb4b6c14ef32501c8abd86c945d4319
Callum <callum.lawsonsmith@gmail.com>
2020-08-07 13:42:35 +1000

    Implement Node to Leverage the Babel Compiler JSX source files from src/ are compiled to js/

---
+++ src/navbar.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const e = React.createElement;
+
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <ul>
+        <a href="index.html"><li>Home</li></a>
+        <a href="about.html"><li>About</li></a>
+        <li>Blog</li>
+        <a href="../docs/Callum-Lawson-Smith-Resume.pdf"><li>Resume</li></a>
+      </ul>
+    );
+  }
+}
+
+ReactDOM.render(<NavBar />, document.querySelector('#navbar'));
